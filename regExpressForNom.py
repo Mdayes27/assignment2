c22f0d323d41d1b13463a07ae6e5f2f5fe7ed434
Mallori <mdayes22@byu.edu>
2019-09-24 11:37:49 -0600

    Update regExpressForNom.py
    Final version

--- regExpressForNom.py
+++ regExpressForNom.py
@@ -1,1 +1,18 @@
-
+han = open('text2Read.py')
+for line in han:
+    line = line.rstrip()
+    wds = line.split()
+    count = 0
+    if wds.endswith("ing"):
+        count++
+        continue
+    elif wds.endswith("or"):
+        count++
+        continue
+    elif wds.endswith("ee"):
+        count++
+        continue
+    elif wds.endswith("ion"):
+        count++
+        continue
+    print("The current count is ",count)
